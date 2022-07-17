306d08fffbba154cbc1a7068ee74c71d89da81fe
= <=>
2022-07-17 11:42:23 +0530

    2.9 complete

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,10 +2,25 @@ import { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "923-233-2343" },
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
+
+  const [filteredPersons, setFilteredPersons] = useState([]);
+  const [searchField, setSearchField] = useState("");
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+
+  const filterNamesHandler = (event) => {
+    console.log(event.target.value);
+    const name = event.target.value;
+    setSearchField(event.target.value);
+    setFilteredPersons(
+      persons.filter((person) => person.name.toLowerCase().includes(name))
+    );
+  };
 
   const inputChangeHandler = (event) => {
     if (event.target.name === "name") {
@@ -29,9 +44,24 @@ const App = () => {
     setNewNumber("");
   };
 
+  const renderPersons =
+    searchField.length > 0
+      ? filteredPersons.map((person) => (
+          <li key={person.id}>
+            {person.name} {person.number}
+          </li>
+        ))
+      : persons.map((person) => (
+          <li key={person.id}>
+            {person.name} {person.number}
+          </li>
+        ));
+
   return (
     <div>
       <h2>Phonebook</h2>
+      filter shown with: <input onChange={filterNamesHandler} />
+      <h2>add a new</h2>
       <form onSubmit={submitHandler}>
         <div>
           name:{" "}
@@ -49,13 +79,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      <ul>
-        {persons.map((person) => (
-          <li key={person.name}>
-            {person.name} {person.number}
-          </li>
-        ))}
-      </ul>
+      <ul>{renderPersons}</ul>
     </div>
   );
 };
